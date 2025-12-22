2d2fa6933d086dbafbeda5d48548f6c547c43b66
ahmadanwar93 <ahmadfikri.anwarfadzil@gmail.com>
2025-12-22 19:57:11 +0800

    fix: verify email async

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,8 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-
 use App\Enums\UserRole;
+use App\Notifications\QueuedVerifyEmail;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -51,6 +50,11 @@ class User extends Authenticatable implements MustVerifyEmail
             'password' => 'hashed', // for auto hashing when saving password
             'role' => UserRole::class,
         ];
+    }
+
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify(new QueuedVerifyEmail());
     }
 
     public function jobs()
